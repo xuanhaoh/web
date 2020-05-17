3f1b7e05c7b96e434b4f620b82b5f8a4c78cd92c
xuanhaoh <xuanhaoh129@gmail.com>
2020-05-17 21:50:00 +1000

    实现twitter和aurin在state的数据处理

--- app/main.py
+++ app/main.py
@@ -3,66 +3,79 @@ from flask import request
 from flask import render_template
 import requests
 import json
-import couchdb
+from collections import OrderedDict
 
 app = Flask(__name__)
 
 address = '172.26.132.81'
 username = 'group63'
 password = '123'
-database = 'twitter_stream_processed'
-view = 'by_place'
-couch = couchdb.Server('http://{}:5984'.format(address))
-couch.resource.credentials = (username, password)
-db = couch[database]
+twitter_database = 'twitter_stream_processed'
+twitter_view = 'by_place'
 
-country = 'Australia'
-state = ['New South Wales', 'Queensland', 'South Australia', 'Tasmania', 'Victoria', 'Western Australia', 'Australian Capital Territory', 'Northern Territory']
-administrative_division = {'Australia': {'New South Wales': ['Sydney', 'Newcastle', 'Wollongong', 'Central Coast'],
-                                         'Queensland': ['Brisbane', 'Cairns', 'Gold Coast', 'Sunshine Coast', 'Toowoomba', 'Townsville'],
-                                         'South Australia': ['Adelaide', 'Port Lincoln'],
-                                         'Tasmania': ['Hobart', 'Launceston'],
-                                         'Victoria': ['Ballarat', 'Bendigo', 'Geelong', 'Melbourne', 'Melton', 'Wangaratta'],
-                                         'Western Australia': ['Albany', 'Perth'],
-                                         'Australian Capital Territory': ['Canberra'],
-                                         'Northern Territory': ['Darwin', 'Ghan']
-                                         }
-                           }
+country_lookup = {'Australia': 8}
+state_lookup = {'NSW': {'full_name': 'New South Wales', 'limit': 8}, 'QLD': {'full_name': 'Queensland', 'limit': 8},
+                'SA': {'full_name': 'South Australia', 'limit': 8}, 'TAS': {'full_name': 'Tasmania', 'limit': 8},
+                'VIC': {'full_name': 'Victoria', 'limit': 8}, 'WA': {'full_name': 'Western Australia', 'limit': 8},
+                'ACT': {'full_name': 'Australian Capital Territory', 'limit': 8},
+                'NT': {'full_name': 'Northern Territory', 'limit': 8}}
+factor_lookup = {'alcohol': {'database': 'aurin_health', 'count': 'alchl_p_1_count', 'rate': 'alchl_p_2_asr'},
+                 'smoking': {'database': 'aurin_health', 'count': 'smkrs_p_1_count', 'rate': 'smkrs_p_2_asr'}}
 
 
 def couch_query(key):
     result = {}
     count = {}
-    response = requests.get('http://{}:5984/{}/_design/{}/_view/{}?group=True'.format(address, database, view, key))
+    response = requests.get('http://{}:5984/{}/_design/{}/_view/{}?group=True'.format(address, twitter_database, twitter_view, key))
     for row in response.json()['rows']:
         result[row['key']] = row['value']['sum'] / row['value']['count']
         count[row['key']] = row['value']['count']
     return result, count
+
 
 @app.route('/')
 def index():
     return render_template('index.html', name='Group 63')
 
 
-@app.route('/sentiment', methods=['POST', 'GET'])
-def sentiment():
-    key = request.args.get("key")
-    result = {}
-    if key == country:
-        response = requests.get('http://{}:5984/{}/_design/{}/_view/{}?group=True'.format(address, database, view, key))
-        for row in response.json()['rows']:
-            if row['key'] in state:
-                result[row['key']] = row['value']['sum'] / row['value']['count']
-    elif key in state:
-        response = requests.get('http://{}:5984/{}/_design/{}/_view/{}?group=True'.format(address, database, view, key))
-        for row in response.json()['rows']:
-            if row['key'] in administrative_division[country][key]:
-                result[row['key']] = row['value']['sum'] / row['value']['count']
-    return render_template('sentiment.html', key=key, result=result)
+@app.route('/backend', methods=['POST', 'GET'])
+def backend():
+    if request.method == 'GET':
+        area = request.args.get("area")
+        factor = request.args.get("factor")
+    elif request.method == 'GET':
+        data = request.get_data()
+        area = data['place']
+        factor = data['factor']
+    else:
+        return 'Method not allowed'
+
+    final_result = OrderedDict()
+    if area in country_lookup.keys():
+        pass
+    elif area in state_lookup.keys():
+        limit = state_lookup[area]['limit']
+        twitter_response = requests.get('http://{}:5984/{}/_design/{}/_view/{}?group=True'
+                                        .format(address, twitter_database, twitter_view, area))
+        twitter_result = {row['key']: {'value': row['value']['sum'] / row['value']['count'],
+                                       'count': row['value']['count']} for row in twitter_response.json()['rows']}
+        ordered_result = OrderedDict(sorted(twitter_result.items(), key=lambda x: x[1]['count'], reverse=True))
+        for k, v in ordered_result.items():
+            aurin_response = requests.post('http://{}:5984/{}/_find'.format(address, factor_lookup[factor]['database']),
+                                           headers={'Content-Type': 'application/json'},
+                                           json={'selector': {'lga_name': {'$regex': r'(Greater )?{} \(\w+\)'.format(k)}},
+                                                 'fields': ['lga_name', factor_lookup[factor]['rate']]})
+            if aurin_response.json()['docs']:
+                aurin_result = aurin_response.json()['docs'][0]
+                final_result[k] = [ordered_result[k]['value'], aurin_result[factor_lookup[factor]['rate']]]
+                if len(final_result) == limit:
+                    return json.dumps(final_result)
+    return json.dumps(final_result)
+
 
 @app.route('/test', methods=['POST', 'GET'])
 def test():
-    if request.method=='POST':
+    if request.method == 'POST':
         loc = request.get_data(as_text=True)
         print(loc)
         d = {}
@@ -87,20 +100,22 @@ def test():
             d, c = couch_query('Northern Territory')
         else:
             d, c = couch_query('South Australia')
-        l = sorted(c.items(), key=lambda item:item[1], reverse=True)
-        if len(l)>8:
+        l = sorted(c.items(), key=lambda item: item[1], reverse=True)
+        if len(l) > 8:
             for i in range(0, 8):
-                tem[l[i][0]] = d[l[i][0]]+0.2  # base +0.2
+                tem[l[i][0]] = d[l[i][0]] + 0.2  # base +0.2
             d = tem
+        print(json.dumps(d, ensure_ascii=False))
         return json.dumps(d, ensure_ascii=False)
 
     else:
         print('456')
         return render_template('smoke.html')
 
+
 @app.route('/smoke', methods=['POST', 'GET'])
 def smoke():
-    if request.method=='POST':
+    if request.method == 'POST':
         d = {"a": 1, "b": 2}
         return render_template('smoke.html', rs=json.dumps(d))
 
