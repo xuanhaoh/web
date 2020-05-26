e583837d5527aee39ccc460f9404012b181b2d17
xuanhaoh <xuanhaoh129@gmail.com>
2020-05-27 02:38:54 +1000

    docker swarm

--- startapp.sh
+++ startapp.sh
@@ -2,4 +2,5 @@
 docker rm web -f
 docker rmi web
 docker build -t web .
-docker run -it -d --name=web -p 80:80 --env IP=$(hostname -I | cut -f 1 -d " ") web
+docker docker service rm web
+docker docker service create --name web -p 80:80 --env IP=$(hostname -I | cut -f 1 -d " ") --replicas 4 web
