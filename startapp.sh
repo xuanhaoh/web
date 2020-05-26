69a6afa1c5c9e1ccf4324175c86f48f694160eed
xuanhaoh <xuanhaoh129@gmail.com>
2020-05-27 02:57:19 +1000

    fix

--- startapp.sh
+++ startapp.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker docker service rm web
-docker docker service create --name web -p 80:80 --env IP=$(hostname -I | cut -f 1 -d " ") --replicas 4 web
+docker service rm web
+docker service create --name web -p 80:80 --env IP=$(hostname -I | cut -f 1 -d " ") --replicas 4 web
