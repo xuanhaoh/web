da612e83daf392976053b3cf02bf4136fd424796
xuanhaoh <xuanhaoh129@gmail.com>
2020-05-26 23:01:33 +1000

    use ip from environment

--- startapp.sh
+++ startapp.sh
@@ -2,4 +2,4 @@
 docker rm web -f
 docker rmi web
 docker build -t web .
-docker run -it -d --name=web -p 80:80 web
+docker run -it -d --name=web -p 80:80 --env IP=$(hostname -I | cut -f 1 -d " ") web
